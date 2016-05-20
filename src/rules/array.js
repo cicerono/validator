2ecbf0395fab1cab36c05d67cd8618d55a1a2e8b
Rolf Erik Lekang <me@rolflekang.com>
2016-05-20 12:22:47 +0200

    style: Remove unnecessary arrow block

--- src/rules/array.js
+++ src/rules/array.js
@@ -3,8 +3,5 @@ import Validator from '../validator';
 
 export function arrayOf(field, value, options) {
   const validator = new Validator(options);
-  return map(value, item => {
-    const result = validator.validate(keys(options), item);
-    return result;
-  });
+  return map(value, item => validator.validate(keys(options), item));
 }
