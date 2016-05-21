8f945c7804f4c275d32abd121dcf5a31d4a89d3b
Rolf Erik Lekang <me@rolflekang.com>
2016-05-21 12:22:06 +0200

    feat: Add date rule (#19)
    Supported options: format, past, future

---
+++ src/rules/date.js
@@ -0,0 +1,18 @@
+import { get } from 'lodash';
+import moment from 'moment';
+
+export function date(field, value, options) {
+  if (!moment(value, get(options, 'format'), true).isValid()) {
+    return 'date/format';
+  }
+
+  if (get(options, 'past') && moment(value).diff(moment(), 'days') >= 0) {
+    return 'date/past';
+  }
+
+  if (get(options, 'future') && moment(value).diff(moment(), 'days') <= 0) {
+    return 'date/future';
+  }
+
+  return null;
+}
