f0118db5d6c06b239d543c8d807605ba0f79816a
Rolf Erik Lekang <me@rolflekang.com>
2016-05-21 10:40:38 +0200

    fix: Make the extended rules accessible to the validator (#18)

--- src/__tests__/extend_tests.js
+++ src/__tests__/extend_tests.js
@@ -1,24 +1,27 @@
-import 'babel-core/register';
 import test from 'ava';
+import { constant } from 'lodash';
 
 import extend from '../extend';
-const newRule = () => {};
+const newRule = constant(null);
 
 test('Validator.extend should extend rules suite', t => {
-  t.plan(2);
   const Validator = extend({ newRule });
   t.truthy(Validator.rules.newRule);
   t.is(Validator.rules.newRule, newRule);
 });
 
 test('Validator.extend should include default rules', t => {
-  t.plan(1);
   const Validator = extend({ newRule });
   t.truthy(Validator.rules.required);
 });
 
 test('Validator.extend should overwrite if the rule has same name', t => {
-  t.plan(1);
   const Validator = extend({ required: newRule });
   t.is(Validator.rules.required, newRule);
+});
+
+test('Validator.extend should create validator that is able to use the new rules', t => {
+  const Validator = extend({ newRule });
+  const result = new Validator({ a: { newRule: true } }).validate(['a'], {});
+  t.deepEqual(result, {});
 });
