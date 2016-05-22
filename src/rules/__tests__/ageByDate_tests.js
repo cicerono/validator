de3970c297db9865d44b40871340ce6e525fae58
Rolf Erik Lekang <me@rolflekang.com>
2016-05-22 13:10:20 +0200

    test: Add tests of ageByDate/exact

--- src/rules/__tests__/ageByDate_tests.js
+++ src/rules/__tests__/ageByDate_tests.js
@@ -4,7 +4,11 @@ import moment from 'moment';
 import { ageByDate } from '../ageByDate';
 
 test('rules.ageByDate should return null for valid values', t => {
+  const two = moment().subtract(2, 'years').format('YYYY-MM-DD');
+
   t.is(ageByDate('field', '2010-05-05', { min: 2 }), null);
+  t.is(ageByDate('field', two, { max: 3 }), null);
+  t.is(ageByDate('field', two, { exact: 2 }), null);
 });
 
 test('rules.ageByDate should return "ageByDate/min" when age is below min', t => {
@@ -14,6 +18,17 @@ test('rules.ageByDate should return "ageByDate/min" when age is below min', t =>
 test('rules.ageByDate should return "ageByDate/max" when age is above max', t => {
   t.is(
     ageByDate('field', moment().subtract(5, 'years').format('YYYY-MM-DD'), { max: 2 }),
+    'ageByDate/max'
+  );
+});
+
+test('rules.ageByDate should return "ageByDate/min" when age is below exact', t => {
+  t.is(ageByDate('field', moment().format('YYYY-MM-DD'), { exact: 18 }), 'ageByDate/min');
+});
+
+test('rules.ageByDate should return "ageByDate/max" when age is above exact', t => {
+  t.is(
+    ageByDate('field', moment().subtract(5, 'years').format('YYYY-MM-DD'), { exact: 2 }),
     'ageByDate/max'
   );
 });
