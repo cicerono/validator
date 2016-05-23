b0985fee6d1504e672a0807b0c8b38f564a95f20
Rolf Erik Lekang <me@rolflekang.com>
2016-05-23 09:05:32 +0200

    fix(rules.arrayOf): Make sure it does not evaluate 'values' as a field
    Since arrayOf uses the keys of options to evaluate which fields to
    include we must omit the metadata fields we add.

--- src/rules/arrayOf.js
+++ src/rules/arrayOf.js
@@ -1,7 +1,7 @@
-import { keys, map } from 'lodash';
+import { keys, map, omit } from 'lodash';
 import Validator from '../validator';
 
 export function arrayOf(field, value, options) {
   const validator = new Validator(options);
-  return map(value, item => validator.validate(keys(options), item));
+  return map(value, item => validator.validate(keys(omit(options, 'values')), item));
 }
