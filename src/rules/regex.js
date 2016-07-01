98f4d7a52383fad1683d9b6328ae21563bc2e15a
Rolf Erik Lekang <me@rolflekang.com>
2016-07-01 14:43:11 +0200

    fix: Make regex rule actually use the pattern

--- src/rules/regex.js
+++ src/rules/regex.js
@@ -1,7 +1,7 @@
 import { isString } from 'lodash';
 
 export function regex(field, value, options) {
-  const pattern = isString(options.pattern) ? new RegExp() : options.pattern;
+  const pattern = isString(options.pattern) ? new RegExp(options.pattern) : options.pattern;
 
   if (!pattern.test(value)) {
     return 'regex';
