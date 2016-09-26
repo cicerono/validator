fe0a965765e9ef8143234e391cadee107ba20ca7
Rolf Erik Lekang <me@rolflekang.com>
2016-09-26 14:22:29 +0200

    fix: Export correct value for list of rules

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ module.exports.default = require('./validator').default;
 module.exports.Validator = require('./validator').default;
 
 module.exports.extend = require('./extend').default;
-module.exports.rules = require('./rules').default;
+module.exports.rules = require('./rules');
