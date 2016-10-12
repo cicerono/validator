334ae555679b567f1161bf0a444665d6773a98f4
Michael Johansen <michaeljohansen83@gmail.com>
2016-10-12 16:10:28 +0200

    fix: require should accept spaces as valid

--- src/rules/required.js
+++ src/rules/required.js
@@ -3,9 +3,7 @@ import { isArray, isEmpty, isString, isNumber, isBoolean } from 'lodash';
 export default function required(field, value) {
   let result = false;
 
-  if (isString(value)) {
-    result = !!value.trim();
-  } else if (isArray(value)) {
+  if (isArray(value)) {
     result = !isEmpty(value);
   } else if (isNumber(value)) {
     result = !!value.toString();
