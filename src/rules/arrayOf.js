aa537144e440dd6744a7b5c13ca4844832815037
Pavol Rajzak <rapasoft@gmail.com>
2016-11-11 13:39:04 +0100

    fix: ArrayOf validation should return null if the validation succeeds (#59)
    Fixes #57

--- src/rules/arrayOf.js
+++ src/rules/arrayOf.js
@@ -1,7 +1,13 @@
-import { keys, map, omit } from 'lodash';
+import { keys, map, omit, isEmpty, reject } from 'lodash';
 import Validator from '../validator';
+
+const isEmptyOrAllNull = (array) => {
+  return isEmpty(array) || isEmpty(reject(array, isEmpty));
+};
 
 export default function arrayOf(field, value, options) {
   const validator = new Validator(options);
-  return map(value, item => validator.validate(keys(omit(options, 'values')), item));
+  const result = map(value, item => validator.validate(keys(omit(options, 'values')), item));
+
+  return isEmptyOrAllNull(result) ? null : result;
 }
