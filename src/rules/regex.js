81607c90e89b4e19a3075458c55952ad3e6407e8
Rolf Erik Lekang <me@rolflekang.com>
2017-02-23 12:20:34 +0100

    fix: Firefox regex issue
    There is an issue where some regexes will trigger an error in firefox
    when they are created with the short format /regex/. Wrapping them in
    new RegExp() mitigates this issue.

--- src/rules/regex.js
+++ src/rules/regex.js
@@ -1,10 +1,8 @@
 // @flow
-import { isString } from 'lodash';
-
 import type { RuleOptions } from '../types';
 
 export default function regex(field: string, value: string, options: RuleOptions): ?string {
-  const pattern = isString(options.pattern) ? new RegExp(options.pattern) : options.pattern;
+  const pattern = new RegExp(options.pattern);
 
   if (!pattern.test(value)) {
     return 'regex';
