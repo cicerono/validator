587089273162ee52f6c0c78de49309466f1b9461
Rolf Erik Lekang <me@rolflekang.com>
2017-02-23 12:20:34 +0100

    fix: Firefox regex issue
    There is an issue where some regexes will trigger an error in firefox
    when they are created with the short format /regex/. Wrapping them in
    new RegExp() mitigates this issue.

--- src/rules/__tests__/regex_tests.js
+++ src/rules/__tests__/regex_tests.js
@@ -2,18 +2,21 @@
 /* eslint-env jest */
 import regex from '../regex';
 
-it('rules.regex it should return null when the value match the pattern', () => {
-  expect(regex('field', '2013', { pattern: /20\d+\d+/ })).toBe(null);
+describe('rules.regex', () => {
+  it('should return null when the value match the pattern', () => {
+    expect(regex('field', '2013', { pattern: /20\d+\d+/ })).toBe(null);
+  });
+
+  it('should return null when the value match a string pattern', () => {
+    expect(regex('field', '2013', { pattern: '20\\d+\\d+' })).toBe(null);
+  });
+
+  it('should return "regex" when the value does not match the pattern', () => {
+    expect(regex('field', '20a3', { pattern: /20\d+\d+/ })).toBe('regex');
+  });
+
+  it('should return "regex" when the value does not match a string pattern', () => {
+    expect(regex('field', '20a3', { pattern: '20\\d+\\d+/' })).toBe('regex');
+  });
 });
 
-it('rules.regex it should return null when the value match a string pattern', () => {
-  expect(regex('field', '2013', { pattern: '20\\d+\\d+' })).toBe(null);
-});
-
-it('rules.regex it should return "regex" when the value does not match the pattern', () => {
-  expect(regex('field', '20a3', { pattern: /20\d+\d+/ })).toBe('regex');
-});
-
-it('rules.regex it should return "regex" when the value does not match a string pattern', () => {
-  expect(regex('field', '20a3', { pattern: '20\\d+\\d+/' })).toBe('regex');
-});
